2c6dca988af55d1d80966a23f7a6050e0cecd984
Julian Osborne <subaruforge@gmail.com>
2017-08-22 21:35:35 -0400

    Update Utils.java

--- main/java/AiRoot/Utils.java
+++ main/java/AiRoot/Utils.java
@@ -1,61 +1,21 @@
 package AiRoot;
 
-import java.io.*;
 import java.util.Random;
-import java.util.StringJoiner;
 
-import static AiRoot.Utils.*;
+class Utils {
 
-public class Read {
-
-    private static StringBuilder contents;
-
-   void getContents(String dir) {
-        contents = new StringBuilder();
-        try {
-            File aFile = new File(dir);
-            try (BufferedReader input = new BufferedReader(new FileReader(aFile))) {
-                String line;
-                while ((line = input.readLine()) != null) {
-                    contents.append(line);
-                    contents.append("\n");
-                }
-            }
-        } catch (IOException ex){
-            ex.printStackTrace();
-        }
-   }
-
-    private String getFallback() {
-        String[] messages = contents.toString().split("\n");
-        StringJoiner sb = new StringJoiner("\n");
-        for (String message : messages) {
-            if (message.startsWith("[fallback:")) {
-                sb.add(GetFallback(message));
-            }
-        }
-        String[] array = sb.toString().split("\n");
-        Random r = new Random();
-        int random = r.nextInt(array.length);
-        return array[random];
+    static int Random(String[] array) {
+        return new Random().nextInt(array.length);
     }
-    String getResponse(String msg){
-       String result;
-       String[] messages = contents.toString().split("\n");
-        StringJoiner sb = new StringJoiner("\n");
-        for (String message : messages) {
-            if (message.startsWith("<" + msg + ">")) {
-                sb.add(GetMessage(message, msg));
-            }
-        }
-        if(sb.toString().isEmpty()) {
-            result = getFallback();
-        } else {
-            String[] array = sb.toString().split("\n");
-            int random = Random(array);
-            result = array[random];
-        }
-        return result;
+
+    static String GetInput(String str, String input){
+        return str.substring(str.length()-2-input.length()).replace(">", "");
+    }
+    static String GetMessage(String str, String input){
+        return str.replaceAll("<"+input+">", "");
+    }
+    static String GetFallback(String str){
+        //int msg_length = str.length()-input.length()*2-4;
+        return str.replace("[fallback:", "").replace("]","");
     }
 }
-
